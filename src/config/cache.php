3b27060efb032c6ac79a764e5b088c101be861d8
rahimi <m.rahimi@azmagroup.ir>
2025-09-05 20:16:38 +0330

    Modify config file
    Add whitelist for filter domains

--- src/config/cache.php
+++ src/config/cache.php
@@ -2,5 +2,10 @@
 
 return [
     'type' => 'file', // or Redis
-    'lifetime' => 3600, // an hour
+    'ttl' => 3600, // an hour
+
+    'filter_url_status' => true,
+    'whitelist' => [
+        'zommit.ir',
+    ],
 ];
