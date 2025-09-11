9ea867492a3fd33736417802b00c9371ed19b69c
rahimi <m.rahimi@azmagroup.ir>
2025-09-11 23:12:53 +0330

    Modify config file

--- src/config/cache.php
+++ src/config/cache.php
@@ -3,9 +3,12 @@
 return [
     'type' => 'file', // or Redis
     'ttl' => 3600, // an hour
+    'timeout' => 30, // seconds
+    'max_content_length' => 10485760, // 10MB
 
     'filter_url_status' => true,
     'whitelist' => [
-        'zommit.ir',
+        'zoomit.ir',
+        'google.com',
     ],
 ];
