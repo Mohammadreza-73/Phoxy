920580c9c14b844bfb9e759af66cc7bd6b2833ba
rahimi <m.rahimi@azmagroup.ir>
2025-11-21 14:26:45 +0330

    Add trait for format bytes

---
+++ src/Concern/Formatable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Phoxy\Concern;
+
+trait Formatable
+{
+    public function FormatBytes(int $bytes, int $precision = 2)
+    {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+        $bytes /= pow(1024, $pow);
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    } 
+}
